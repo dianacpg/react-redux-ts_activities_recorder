310431ae54a7afbef45e4dc64e8b2a13f5508d77
dianacpg <dianacpgouveia@gmail.com>
2021-09-23 21:29:40 +0100

    add userevents

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -18,6 +18,20 @@ const Calendar: React.FC = () => {
           </div>
         </div>
       </div>
+      <div className="calendar-day">
+        <div className="calendar-day-label">
+          <span> 1 February</span>
+        </div>
+        <div className="calendar-events">
+          <div className="calendar-event">
+            <div className="calendar-event-info">
+              <div className="calendar-event-time">10:00 -12:00</div>
+              <div className="calendar-event-title"> Learning TypeScript</div>
+            </div>
+            <button className="calendar-event-delete-button">&times;</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
