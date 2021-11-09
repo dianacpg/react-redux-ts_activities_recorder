8019e2f93fe4c96ecce86b1f953702358b1d81b8
dianacpg <dianacpgouveia@gmail.com>
2021-11-09 19:33:27 +0000

    add editing titles

--- src/components/Calendar/EventItem.tsx
+++ src/components/Calendar/EventItem.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { deleteUserEvent, UserEvent } from '../redux/user-events';
+import React, { useState, useEffect, useRef } from 'react';
+import { deleteUserEvent, UserEvent, updateUserEvent } from '../redux/user-events';
 import { useDispatch } from 'react-redux';
 
 interface Props {
@@ -11,11 +11,46 @@ const EventItem: React.FC<Props> = ( {event }) => {
     const handleDeleteClick= () => {
         dispatch(deleteUserEvent(event.id));
     }
+    const [editable, setEditable] = useState(false)
+    const handleTitleClick = () => {
+        setEditable(true)
+    };
+
+    const inputRef = useRef<HTMLInputElement>(null)
+    useEffect (()=> {
+        if (editable) {
+            inputRef.current?.focus();
+        }
+    }, [editable]);
+
+    const [title, setTitle] = useState(event.title);
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.target.value);
+    }
+    const handleBlur = () => {
+        if (title !== event.title) {
+            dispatch(updateUserEvent({
+                ...event,
+                title
+            }));
+        }
+        setEditable(false);
+    }
+    
     return (
         <div key={event.id} className="calendar-event">
         <div className="calendar-event-info">
             <div className="calendar-event-time">10:00 -12:00</div>
-            <div className="calendar-event-title"> {event.title}</div>
+            <div className="calendar-event-title">
+                {editable? (
+                    <input 
+                    type="text" 
+                    ref={inputRef} 
+                    value={title} 
+                    onChange={handleChange}
+                    onBlur={handleBlur}/>
+                ) : (<span onClick={handleTitleClick}>{event.title}</span>)} 
+            </div>
         </div>
         <button className="calendar-event-delete-button" onClick={handleDeleteClick}>&times;</button>
         </div>
